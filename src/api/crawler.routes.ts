95934dbaee38893cd3252b71bed5e3f6619e825a
Wilson Felipe <wfelipe2011@gmail.com>
2022-09-15 19:02:39 -0300

    execute mining profile test

--- src/api/crawler.routes.ts
+++ src/api/crawler.routes.ts
@@ -1,14 +1,40 @@
 import { Router } from 'express';
 
-import logger from '../config/logger';
+import { AutoScroll } from '../entities/AutoScroll';
+import { SleepTime } from '../entities/SleepTime';
+import { PuppeteerAdapter } from '../infra/PuppeteerAdapter';
+import { LoginLinkedin } from '../services/LoginLinkedin';
+import { MiningProfile } from '../services/MiningProfile';
 
 const crawlerRoutes = Router();
+const puppeteerAdapter = new PuppeteerAdapter();
+async function testCrawler(url: string): Promise<void> {
+	const page = await puppeteerAdapter.newPage();
+	const loginLinkedin = new LoginLinkedin(page);
+	await loginLinkedin.execute({
+		login: 'ws2838841@gmail.com',
+		password: 'Mudar123456',
+	});
+	// esperar até que a página carregue
+	await SleepTime.execute(5000);
+	// ir até perfil
+	await page.goto(`https://www.linkedin.com/in/${url}`, {
+		waitUntil: 'domcontentloaded',
+	});
+	// minerar dados
+	const autoScroll = new AutoScroll(page, 100);
+	const mining = new MiningProfile(page, autoScroll);
+	return await mining.execute();
+}
 
-crawlerRoutes.get('/', async (req, res) => {
+crawlerRoutes.get('/:url', async (req, res) => {
 	try {
-		res.send("Hello World");
+		const { url } = req.params;
+		const data = await testCrawler(url);
+		res.send({ data });
 	} catch (error) {
-		logger.error(error);
+		console.error(error);
+		res.send({ data: error });
 	}
 });
 
