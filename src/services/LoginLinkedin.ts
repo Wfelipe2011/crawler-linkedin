82e1f5a38643d34c0a7d15897ac2d51321ec3e9f
Wilson Felipe <wfelipe2011@gmail.com>
2022-09-15 17:13:03 -0300

    added case login linkedin

---
+++ src/services/LoginLinkedin.ts
@@ -0,0 +1,37 @@
+import logger from '../config/logger';
+import { IPage } from '../interfaces/IPage';
+
+const SELECTORS = {
+	url: 'https://www.linkedin.com/login/pt?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin',
+	login: '[id="username"]',
+	password: '[id="password"]',
+	enter: '[aria-label="Entrar"]',
+};
+interface ILoginParams {
+	login: string;
+	password: string;
+}
+
+export class LoginLinkedin {
+	// eslint-disable-next-line prettier/prettier
+	constructor(readonly page: IPage) { }
+
+	async execute(params: ILoginParams): Promise<void> {
+		try {
+			const { login, password } = params;
+			await this.page.goto(SELECTORS.url, {
+				waitUntil: 'domcontentloaded',
+			});
+			await this.page.type(SELECTORS.login, login, {
+				delay: 100,
+			});
+			await this.page.type(SELECTORS.password, password, {
+				delay: 100,
+			});
+			await this.page.click(SELECTORS.enter);
+			logger.info(`Passou do login o job: ${login}`);
+		} catch (error) {
+			logger.error(`Erro no login do job: ${error}`);
+		}
+	}
+}
