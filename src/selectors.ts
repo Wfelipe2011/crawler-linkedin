eb9119333741685792e2b07ba6f77a887e8365d5
Wilson Felipe <wfelipe2011@gmail.com>
2022-09-18 12:00:39 -0300

    added get experience list

--- src/selectors.ts
+++ src/selectors.ts
@@ -7,4 +7,18 @@ export const SELECTORS = {
 			'inline-show-more-text--is-collapsed-with-line-clamp',
 		local: 'text-body-small inline t-black--light break-words',
 	},
+	DOM_REFERENCE: {
+		experience: {
+			title: ` > div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between > div.display-flex.flex-column.full-width > div > span > span:nth-child(1)`,
+			subTitle: ` > div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between > a > div > span > span:nth-child(1)`,
+			description: ` > div > div.display-flex.flex-column.full-width.align-self-center > div.pvs-list__outer-container > ul > li:nth-child(1) > div > ul > li > div > div > div > span:nth-child(1)`,
+			competence: ` > div > div.display-flex.flex-column.full-width.align-self-center > div.pvs-list__outer-container > ul > li:nth-child(2) > div > ul > li > div > div > div > span:nth-child(1)`,
+			company: ` > div > div.display-flex.flex-column.full-width.align-self-center > div > div.display-flex.flex-column.full-width > span:nth-child(2) > span:nth-child(1)`,
+			companyLink: ` > div > div:nth-child(1) > a > div > div > img`,
+			date: ` > div > div.display-flex.flex-column.full-width.align-self-center > div > div.display-flex.flex-column.full-width > span:nth-child(3) > span:nth-child(1)`,
+			date2: ` > div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between > a > span:nth-child(3) > span:nth-child(1)`,
+			location: ` > div > div.display-flex.flex-column.full-width.align-self-center > div > div.display-flex.flex-column.full-width > span:nth-child(4) > span:nth-child(1)`,
+			location2: ` > div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between > a > span:nth-child(4) > span:nth-child(1)`,
+		},
+	},
 };
--- src/services/MiningProfile.ts
+++ src/services/MiningProfile.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import logger from '../config/logger';
 import { AutoScroll } from '../entities/AutoScroll';
+import { SleepTime } from '../entities/SleepTime';
 import { LinkedinHtml } from '../helpers/LinkedinHtml';
 import { IPage } from '../interfaces/IPage';
 import { SELECTORS } from '../selectors';
@@ -35,14 +36,14 @@ export class MiningProfile {
 
 	async execute(): Promise<any> {
 		try {
-			logger.info('Scrolling page...');
-			await this.autoScroll.execute();
-			logger.info('Scrolling done');
+			await this.executeScroll();
 			const profile = await this.getProfileData();
 			const about = await this.getAboutData();
+			const experience = await this.getExperienceData();
 			return {
 				profile,
-				about
+				about,
+				experience
 			};
 		} catch (error) {
 			console.log(error);
@@ -82,5 +83,78 @@ export class MiningProfile {
 		}) as string;
 
 		return LinkedinHtml.sanitize(sectionAboutHtml).replace('…ver mais ', '').replace(' Sobre', '');
+	}
+
+	private async executeScroll(): Promise<void> {
+		await SleepTime.execute(3000);
+		logger.info('Scrolling page...');
+		await this.autoScroll.execute();
+		logger.info('Scrolling done');
+		await SleepTime.execute(3000);
+	}
+
+	private async getExperienceData(): Promise<any> {
+		const urlProfileExperience =  this.page.url() + 'details/experience/';
+		await this.page.goto(urlProfileExperience, {
+			waitUntil: 'domcontentloaded',
+		});
+		
+		await this.executeScroll();
+
+		const sectionExperienceHtml = await this.page.evaluate((DOM_REFERENCE) => {
+			const ul = document.querySelectorAll('[class="pvs-list "]');
+			const teveOutrosCargos = ul.length > 1;
+			const lis = teveOutrosCargos ? ul[1].children : ul[0]?.children;
+			const experience = [];
+			let companyPrincipal = '';
+			let companyLinkPrincipal = '';
+			if(teveOutrosCargos){
+				for(const li of ul[0].children){
+					const id = '#' + li.id;	
+					const companySelector = id + DOM_REFERENCE.experience.subTitle;
+					companyPrincipal = (document.querySelector(companySelector)as HTMLElement)?.innerText;
+					const companyLinkSelector = id + DOM_REFERENCE.experience.companyLink;
+					companyLinkPrincipal = (document.querySelector(companyLinkSelector)as HTMLElement)?.getAttribute('src') || '';
+					}
+			}
+				
+			for (const li of lis) {
+				const id = '#' + li.id;
+				const titleSelector = id + DOM_REFERENCE.experience.title;
+				const title = (document.querySelector(titleSelector)as HTMLElement)?.innerText;
+				const subTitleSelector = id + DOM_REFERENCE.experience.subTitle;
+				const subTitle = (document.querySelector(subTitleSelector)as HTMLElement)?.innerText ;
+				const descriptionSelector = id + DOM_REFERENCE.experience.description;
+				const description = (document.querySelector(descriptionSelector)as HTMLElement)?.innerText; 
+				const competenceSelector = id + DOM_REFERENCE.experience.competence;
+				const competence = (document.querySelector(competenceSelector)as HTMLElement)?.innerText;
+				const companySelector = id + DOM_REFERENCE.experience.company;
+				const company = (document.querySelector(companySelector)as HTMLElement)?.innerText;
+				const companyLinkSelector = id + DOM_REFERENCE.experience.companyLink;
+				const companyLink = (document.querySelector(companyLinkSelector)as HTMLElement)?.getAttribute('src');
+				const dateSelector = id + DOM_REFERENCE.experience.date;
+				const date = (document.querySelector(dateSelector)as HTMLElement)?.innerText;
+				const dateSelector2 = id + DOM_REFERENCE.experience.date2;
+				const date2 = (document.querySelector(dateSelector2)as HTMLElement)?.innerText;
+				const locationSelector = id + DOM_REFERENCE.experience.location;
+				const location = (document.querySelector(locationSelector)as HTMLElement)?.innerText;
+				const locationSelector2 = id + DOM_REFERENCE.experience.location2;
+				const location2 = (document.querySelector(locationSelector2)as HTMLElement)?.innerText;	
+
+				const experienceItem = {
+					title: title ? title : subTitle,
+					description,
+					competence,
+					company: company ? company : companyPrincipal,
+					companyLink: companyLink ? companyLink : companyLinkPrincipal,
+					date: date ? date : date2,
+					location: location ? location : location2
+				};
+				experience.push(experienceItem);
+			}
+			return experience;
+		}, SELECTORS.DOM_REFERENCE) as unknown as string;
+
+		return sectionExperienceHtml;
 	}
 }
